981d29c5ac3db6b78062cf03ea83a93787d39857
Sebitass47 <smdlr47@hotmail.com>
2022-06-07 13:49:46 -0500

    Aplicación terminada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './Styles/App.scss';
+import patternDesktop from './pattern-divider-desktop.svg'
+import patterMobile from './pattern-divider-mobile.svg'
+import iconDice from './icon-dice.svg'
+import useScreenSize from'./Hooks/useScreenSize'
 
 function App() {
+  const [advice, setAdvice] = useState({id: 117, advice: "It is easy to sit up and take notice, what's difficult is getting up and taking action."})
+  let width = useScreenSize()
+
+  let newAdvice = async () => {
+    const response = await fetch('https://api.adviceslip.com/advice')
+    const data = await response.json()
+    setAdvice({id: data.slip.id, advice: data.slip.advice})
+
+  }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App">
+        <div className='container'>
+          <h1 className='title'>ADVICE #{advice['id']}</h1>
+          <p className='advice'>"{advice['advice']}"</p>
+          <img className='pattern-divider' srcSet={width > 1100 ? patternDesktop: patterMobile } alt='Pattern Divider'></img>
+        </div>
+        <button className='dice' onClick={newAdvice}><img src={iconDice} alt='Dice'/></button>
       </header>
-    </div>
   );
 }
 
